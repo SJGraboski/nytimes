139492283f08dd0e9a526663df25ef603ecdb80f
M F <M F>
2016-03-05 14:03:46 -0500

    final?

--- assets/js/api.js
+++ assets/js/api.js
@@ -1,24 +1,24 @@
 
 
  $("#search").on('click', function() {
- 	var searchTerm = $("#search-term").val(); 
-	var numRecord = $("#number-responses").val();
-	var startYear = $("#start-year").val();
-	var endYear = $("#end-year").val();
+ 	var searchTerm = $("#search-term").val().trim(); 
+	var numRecord = $("#number-responses").val().trim();
+	var startYear = $("#start-year").val().trim();
+	var endYear = $("#end-year").val().trim();
 
 	var queryURL = "http://api.nytimes.com/svc/search/v2/articlesearch.json?q=" + 
 		searchTerm + "&begin_date=" + startYear + "0101&end_date=" + endYear + 
-	"	1231&page=0&api-key=4d967c5d0ae05680f9bd295982d1e0cd%3A5%3A74629249";
+		"1231&page=0&api-key=4d967c5d0ae05680f9bd295982d1e0cd%3A5%3A74629249";
 	console.log(queryURL);
 
 	$.ajax({
 		url: queryURL,
 		method: 'GET'
 			})
-		.done(function(response) {
+		.done(function(search) {
 
-			var results = response.docs;
-
+			var results = search.response.docs;
+			console.log(results);
 			for(i=0;i<numRecord;i++) {
 				var articleDiv = $('<div class="item">');
 
@@ -26,23 +26,26 @@
 				var articleNum = $('<span>').text(i+1);
 				var headlineText = results[i].headline.main;
 				articleHeader.append(articleNum, headlineText)
-				var author = $('<p>').text(results[i].byline.person.firstname + results[i].byline.person.lastname);
+				var author = $('<p>').text(results[i].byline.person[0].firstname + " " + results[i].byline.person[0].lastname);
 				var section = $('<p>').text(results[i].section_name);
 				var datePublished = $('<p>').text(results[i].pub_date);
 				var articleLink = $('<p>').text(results[i].web_url);
 
 				articleDiv.append(articleHeader, author, section, datePublished, articleLink);
+				console.log(articleDiv);
 				$("#article-area").append(articleDiv);
 			}
+
+		});
 			
 
     	});
 
 
-	 }
+	
 
 
 $("#clear-response").on('click', function() {
 	$("#article-area").empty();
-}
+});
 
